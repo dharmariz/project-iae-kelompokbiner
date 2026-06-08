205f16fad929d4ee7f20f97c63838f9c2d0858a6
sweetyblue <vivianamaharani6@gmail.com>
2026-06-08 14:39:08 +0700

    Setup notification service with RabbitMQ consumer

--- notification-service/routes/web.php
+++ notification-service/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Jobs\ReservationCreatedJob;
+
+Route::get('/test-rabbitmq', function () {
+
+    ReservationCreatedJob::dispatch([
+        'user_id' => 1
+    ]);
+
+    return 'Message Sent!';
+});
 Route::get('/', function () {
     return view('welcome');
 });
